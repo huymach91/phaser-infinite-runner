a3f2a1b21e71c0b465b836a53af8e7390b5d0dcf
huymach91 <huy.mach.webdev@gmail.com>
2021-12-02 15:45:28 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-bdb4ym
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-bdb4ym)
---
+++ index.ts
@@ -0,0 +1,18 @@
+import Phaser from 'phaser';
+// we can delete this line
+// import HelloWorldScene from './scenes/HelloWorldScene// add this line to import Game
+import Game from './scenes/game';
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 640,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  // replace HelloWorldScene with Game in the 'scene
+  scene: [Game],
+};
+export default new Phaser.Game(config);
