7092c5b274b98d831e3af4580b4a3f8f8e6c3749
huymach91 <huy.mach.webdev@gmail.com>
2021-12-02 16:06:58 +0700

    2 files modified, 1 added

--- scenes/game.ts
+++ scenes/game.ts
@@ -1,9 +1,13 @@
 import Phaser from 'phaser';
+
 export default class Game extends Phaser.Scene {
   constructor() {
     super('game');
   }
 
-  preload() {}
+  preload() {
+    this.load.image('background', 'house/bg_repeat_340x640.png');
+  }
+
   create() {}
 }
