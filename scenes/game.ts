ea8e25a8d4020a4a866aee00bb6478319afaa554
huymach91 <huy.mach.webdev@gmail.com>
2021-12-02 22:19:51 +0700

    1 binaries uploaded

--- scenes/game.ts
+++ scenes/game.ts
@@ -1,6 +1,8 @@
 import * as Phaser from 'phaser';
 
 export default class Game extends Phaser.Scene {
+  private player: any;
+
   constructor() {
     super('game');
   }
@@ -10,7 +12,11 @@ export default class Game extends Phaser.Scene {
       'background',
       'https://cdn.jsdelivr.net/gh/huymach91/infinite-runner@master/assets/bg_repeat_340x640.png'
     );
-    this.load.spritesheet('player', '');
+    this.load.atlas(
+      'player',
+      'https://raw.githubusercontent.com/huymach91/phaser-infinite-runner/master/assets/rocketmouse_run01.png',
+      
+    );
   }
 
   create() {
@@ -18,5 +24,7 @@ export default class Game extends Phaser.Scene {
     const height = this.scale.height;
 
     this.add.tileSprite(0, 0, width, height, 'background').setOrigin(0, 0);
+
+    this.player = this.add.sprite(50, 50, 'player');
   }
 }
