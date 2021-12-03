2b489e41f31825fe56cf52b80ed89fb3529c9487
huymach91 <huy.mach.webdev@gmail.com>
2021-12-03 21:35:18 +0700

    1 file modified

--- scenes/game.ts
+++ scenes/game.ts
@@ -27,7 +27,7 @@ export default class Game extends Phaser.Scene {
     this.add.tileSprite(0, 0, width, height, 'background').setOrigin(0, 0);
 
     this.add.sprite(
-      width * 0.5, // middle of screen
+      width * 0.5,
       height * 0.5,
       'rocket-mouse',
       'rocketmouse_fly01.png'
