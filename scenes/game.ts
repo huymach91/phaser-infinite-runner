ba9abc3c4529d78f0245d662dd998669f6a67671
huymach91 <huy.mach.webdev@gmail.com>
2021-12-05 21:41:44 +0700

    1 file modified

--- scenes/game.ts
+++ scenes/game.ts
@@ -26,11 +26,26 @@ export default class Game extends Phaser.Scene {
 
     this.add.tileSprite(0, 0, width, height, 'background').setOrigin(0, 0);
 
-    this.add.sprite(
+    this.player = this.add.sprite(
       width * 0.5,
       height * 0.5,
       'rocket-mouse',
       'rocketmouse_fly01.png'
     );
+
+    this.anims.create({
+      key: 'rocket-mouse-run',
+      frames: this.anims.generateFrameNames('rocket-mouse', {
+        start: 1,
+        end: 4,
+        prefix: 'rocketmouse_run',
+        zeroPad: 2,
+        suffix: '.png',
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.player.play('rocket-mouse-run');
   }
 }
